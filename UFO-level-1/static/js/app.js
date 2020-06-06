1928d506c62d86a9556095ddba33fbcf789060c6
adrianavandeguchte <61213204+adrianavandeguchte@users.noreply.github.com>
2020-06-06 13:31:57 -0400

    updates to index.html and app.js

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -43,6 +43,7 @@ tableData.forEach((ufoReport) => {
     //rewrite table
 // Select the button
 var button = d3.select("#filter-btn");
+var reset = d3.select("#reset-btn");
 
 // Select the form
 var form = d3.select("#form");
@@ -50,6 +51,8 @@ var form = d3.select("#form");
 // Create event handlers for clicking the button or pressing the enter key
 button.on("click", runEnter);
 form.on("submit",runEnter);
+reset.on("click",runReset);
+
 
 // Create the function to run for both events
 function runEnter() {
@@ -57,14 +60,61 @@ function runEnter() {
   // Prevent the page from refreshing
   d3.event.preventDefault();
 
+  // Select dropdown menu
+  var dropdownMenu = d3.select("#selFilter");
+  var searchFilter = dropdownMenu.property("value");
+  var searchID = "#"+searchFilter
+  console.log(searchFilter);
+
   // Select the input element and get the raw HTML node
-  var inputElement = d3.select("#example-form-input");
+  var inputElement = d3.select(searchID);
 
   // Get the value property of the input element
   var inputValue = inputElement.property("value");
 
+
+
   // Use input value to filter tableData
-  
+  function datetimeFilter(td) {
+    return td.searchID == inputValue;
+  };
+  var filteredData = tableData.filter(datetimeFilter);
+  console.log(filteredData);
+  // empties the table
+  document.getElementById("tableContent").innerHTML = "";
+
+  // Refills table with filtered data
+  filteredData.forEach((ufoReport) => {
+    var row = tbody.append("tr");
+    Object.entries(ufoReport).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
+
+};
+
+// reset table filters buttons
+function runReset() {
+  // Prevent the page from refreshing
+  d3.event.preventDefault();
+
+  // clears table
+  document.getElementById("tableContent").innerHTML = "";
+
+  // loop to add data into table
+  tableData.forEach((ufoReport) => {
+    var row = tbody.append("tr");
+    Object.entries(ufoReport).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
+
+  // resets filter input box to original example text
+  //document.getElementById("datetime").reset();
+};
+
 // Level 2: Multiple Search Categories (Optional)
 // Complete all of Level 1 criteria.
 //
